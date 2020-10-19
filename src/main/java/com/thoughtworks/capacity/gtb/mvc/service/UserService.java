184fc1af1bfa2b5de308dc10ddef14d97c71297f
imdreamer <743295483@qq.com>
2020-10-19 16:25:57 +0800

    [yangqian] feat: add register service and test.

--- src/main/java/com/thoughtworks/capacity/gtb/mvc/service/UserService.java
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/service/UserService.java
@@ -1,7 +1,21 @@
 package com.thoughtworks.capacity.gtb.mvc.service;
 
+import com.thoughtworks.capacity.gtb.mvc.Repository.UserRepository;
+import com.thoughtworks.capacity.gtb.mvc.dto.User;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void register(User user) {
+        user.setId(userRepository.getMaxUserId());
+        userRepository.save(user);
+        userRepository.setMaxUserId();
+    }
 }
--- src/test/java/com/thoughtworks/capacity/gtb/mvc/ServiceTests/UserServiceTest.java
+++ src/test/java/com/thoughtworks/capacity/gtb/mvc/ServiceTests/UserServiceTest.java
@@ -1,7 +1,41 @@
 package com.thoughtworks.capacity.gtb.mvc.ServiceTests;
 
+import com.thoughtworks.capacity.gtb.mvc.Repository.UserRepository;
+import com.thoughtworks.capacity.gtb.mvc.dto.User;
+import com.thoughtworks.capacity.gtb.mvc.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
 
 @SpringBootTest
 public class UserServiceTest {
+
+    UserService userService;
+    @Mock
+    UserRepository userRepository;
+
+    @BeforeEach
+    void setUp() {
+        initMocks(this);
+        userService = new UserService(userRepository);
+    }
+
+    @Test
+    void should_return_none_body_when_register_user_success() {
+        User user = User.builder()
+                .username("yangqian")
+                .password("123")
+                .email("743295483@qq.com")
+                .build();
+        when(userRepository.findByUsername(user.getUsername())).thenReturn(Optional.empty());
+        userService.register(user);
+        verify(userRepository).save(user);
+    }
 }
