dbcb814f79973244b8217ae5e5dd51b7a26595dd
imdreamer <743295483@qq.com>
2020-10-19 15:37:09 +0800

    [yangqian] init: project.

---
+++ src/test/java/com/thoughtworks/capacity/gtb/mvc/ControllerTests/UserControllerTest.java
@@ -0,0 +1,7 @@
+package com.thoughtworks.capacity.gtb.mvc.ControllerTests;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserControllerTest {
+}
---
+++ src/test/java/com/thoughtworks/capacity/gtb/mvc/ServiceTests/UserServiceTest.java
@@ -0,0 +1,7 @@
+package com.thoughtworks.capacity.gtb.mvc.ServiceTests;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserServiceTest {
+}
